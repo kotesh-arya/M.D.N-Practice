8ad8542a050d24993f531511f7048b1a3ce7ba77
kotesh-arya <koteswarraomudila@gmail.com>
2021-02-25 22:47:57 +0530

    updated starter files
    added content to initiated files

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 
 // content updating using a function
-const para = document.querySelector("p");
-para.addEventListener('click',updateName);
+// const para = document.querySelector("p");
+// para.addEventListener('click',updateName);
  
 function updateName(){
     let name = prompt("Enter the first player name");
     para.textContent = "Player 1 :" + name;
+}
+
+
+const buttons = document.querySelectorAll('button');
+for(i=0;i<buttons.length;i++){
+    var content = document.querySelector('p');
+    content.textContent = "this is a paraghraph created that"
+    buttons[i].addEventListener('click',updateName);
+
 }
